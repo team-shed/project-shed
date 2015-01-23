84f74001b44ae2ccac5826830184d9677b7bb273
Lucariatias <lucariatias@gmail.com>
2015-01-23 19:47:51 +0000

    Initial commit

---
+++ html/src/com/echospiral/projectshed/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.echospiral.projectshed.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.echospiral.projectshed.ProjectShed;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new ProjectShed();
+        }
+}
