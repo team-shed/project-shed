a7d5c5c44e6ae6bd10e0c541a3c346a7ba81f116
winterismute <rudibonfiglioli@gmail.com>
2015-01-23 23:43:24 +0100

    - Added WorldObjectsGroup - Changed collision system a bit

--- core/src/com/echospiral/projectshed/object/WorldObject.java
+++ core/src/com/echospiral/projectshed/object/WorldObject.java
@@ -5,9 +5,29 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.echospiral.projectshed.world.World;
 
+import java.util.concurrent.Callable;
+
 import static java.lang.Math.round;
 
 public abstract class WorldObject {
+
+    public static class WorldObjectsOnCollisionCallback implements Callable<Boolean> {
+        private WorldObject object1;
+        private WorldObject object2;
+
+        public WorldObjectsOnCollisionCallback() {
+            this.object1 = null;
+            this.object2 = null;
+        }
+
+        public void setArguments(WorldObject o1, WorldObject o2) {
+            this.object1 = o1;
+            this.object2 = o2;
+        }
+        public Boolean call() {
+            return false;
+        }
+    }
 
     private World world;
     private int x;
@@ -15,31 +35,117 @@ public abstract class WorldObject {
     private int dx;
     private int dy;
 
+    private boolean hasCollidedRight;
+    private boolean hasCollidedLeft;
+    private boolean hasCollidedTop;
+    private boolean hasCollidedBottom;
+
     public WorldObject(World world, int x, int y) {
         this.world = world;
         this.x = x;
         this.y = y;
+
+        hasCollidedLeft = false;
+        hasCollidedRight = false;
+        hasCollidedTop = false;
+        hasCollidedBottom = false;
     }
 
+    // TODO: fixme, this should be called update or update move
     public void tick(float delta) {
-        boolean xFree = true;
+
+        if (!isHorizontallyBlocked())
+            setX(getX() + (int) round((double) dx * delta));
+
+        if (!isVerticallyBlocked())
+            setY(getY() + (int) round((double) dy * delta));
+
+        resetCollisionFlags();
+    }
+
+    protected boolean isHorizontallyBlocked() {
+        return hasCollidedLeft || hasCollidedRight;
+    }
+
+    protected  boolean isVerticallyBlocked() {
+        return  hasCollidedBottom || hasCollidedTop;
+    }
+
+    protected void resetCollisionFlags() {
+        hasCollidedLeft = false;
+        hasCollidedRight = false;
+        hasCollidedTop = false;
+        hasCollidedBottom = false;
+    }
+
+    public boolean collideWith(WorldObject object) {
+        return  collideWith(object, null);
+    }
+
+    public <T extends WorldObjectsOnCollisionCallback> boolean collideWith(WorldObject object, T toCall) {
+        boolean anyCollision = false;
         if (dx != 0) {
-            for (WorldObject object : world.getObjects()) {
-                if (getRelativeBounds(dx, 0).overlaps(object.getRelativeBounds(0, 0))) {
-                    xFree = false;
-                }
+            if (getRelativeBounds(dx, 0).overlaps(object.getRelativeBounds(0, 0))) {
+                hasCollidedLeft = true;
+                hasCollidedRight = true;
+                anyCollision = true;
             }
         }
-        boolean yFree = true;
+
         if (dy != 0) {
-            for (WorldObject object : world.getObjects()) {
-                if (getRelativeBounds(0, dy).overlaps(object.getRelativeBounds(0, 0))) {
-                    yFree = false;
-                }
+            if (getRelativeBounds(0, dy).overlaps(object.getRelativeBounds(0, 0))) {
+                hasCollidedBottom = true;
+                hasCollidedTop = true;
+                anyCollision = true;
             }
         }
-        if (xFree) setX(getX() + (int) round((double) dx * delta));
-        if (yFree) setY(getY() + (int) round((double) dy * delta));
+
+        if (toCall != null) {
+            toCall.setArguments(this, object);
+            toCall.call();
+        }
+
+        return anyCollision;
+    }
+
+    public boolean collideWith(WorldObjectsGroup group) {
+        return  this.collideWith(group, null);
+    }
+
+    public <T extends WorldObjectsOnCollisionCallback> boolean collideWith(WorldObjectsGroup group, T toCall) {
+        boolean anyCollision = false;
+        boolean anyCollisionWhole = false;
+
+        for (WorldObject o : group.getGroupObjects()) {
+            anyCollision = false;
+
+            if (dx != 0) {
+                if (getRelativeBounds(dx, 0).overlaps(o.getRelativeBounds(0, 0))) {
+                    // TODO: fixme, right direction
+                    hasCollidedLeft = true;
+                    hasCollidedRight = true;
+                    anyCollision = true;
+                }
+            }
+
+            if (dy != 0) {
+                if (getRelativeBounds(0, dy).overlaps(o.getRelativeBounds(0, 0))) {
+                    // TODO: fixme, right direction
+                    hasCollidedBottom = true;
+                    hasCollidedTop = true;
+                    anyCollision = true;
+                }
+            }
+
+            if (toCall != null && anyCollision) {
+                toCall.setArguments(this, o);
+                toCall.call();
+            }
+
+            anyCollisionWhole = anyCollisionWhole || anyCollision;
+        }
+
+        return  anyCollisionWhole;
     }
 
     public World getWorld() {
