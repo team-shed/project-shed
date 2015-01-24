2b3d322d9f00c6df55368b72203392b38bdf9322
Jeffrey Kyllo <jkyllo@echospiral.com>
2015-01-24 12:57:18 +0000

    Removed splash to ease development.   No really.

--- core/src/com/echospiral/projectshed/ProjectShed.java
+++ core/src/com/echospiral/projectshed/ProjectShed.java
@@ -23,7 +23,8 @@ public class ProjectShed extends Game {
         splashImage.setCenter(512, 384);
 
         GameScreen gameScreen = new GameScreen(this);
-        this.setScreen(new SplashScreen(this, gameScreen, splashImage, 0.5f, 1.5f, 2.0f));
+        //this.setScreen(new SplashScreen(this, gameScreen, splashImage, 0.5f, 1.5f, 2.0f));
+        this.setScreen(gameScreen);
 	}
 
     public SpriteBatch getSpriteBatch() {
