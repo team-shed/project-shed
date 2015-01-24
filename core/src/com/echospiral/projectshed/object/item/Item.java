84d8cf7e6680f92c74aeac6342e7747aad093782
alyphen <alyphen@gmail.com>
2015-01-24 10:30:41 +0000

    Add items

---
+++ core/src/com/echospiral/projectshed/object/item/Item.java
@@ -0,0 +1,45 @@
+package com.echospiral.projectshed.object.item;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.echospiral.projectshed.object.WorldObject;
+import com.echospiral.projectshed.world.World;
+
+public abstract class Item extends WorldObject {
+
+    private Animation animation;
+    private float stateTime;
+
+    public Item(World world, int x, int y, Animation animation) {
+        super(world, x, y);
+        this.animation = animation;
+        stateTime = 0F;
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch, ShapeRenderer shapeRenderer) {
+        spriteBatch.draw(getAnimation().getKeyFrame(stateTime), getX(), getY());
+    }
+
+    @Override
+    public void tick(float delta) {
+        super.tick(delta);
+        stateTime += delta;
+    }
+
+    @Override
+    public Rectangle getRelativeBounds(int dx, int dy) {
+        TextureRegion frame = getAnimation().getKeyFrame(stateTime);
+        return new Rectangle(getX() + dx, getY() + dy, frame.getRegionWidth(), frame.getRegionHeight());
+    }
+
+    public Animation getAnimation() {
+        return animation;
+    }
+
+    public abstract void activate();
+
+}
--- core/src/com/echospiral/projectshed/world/World.java
+++ core/src/com/echospiral/projectshed/world/World.java
@@ -3,18 +3,39 @@ package com.echospiral.projectshed.world;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.Array;
+import com.echospiral.projectshed.object.Block;
 import com.echospiral.projectshed.object.WorldObject;
+import com.echospiral.projectshed.object.WorldObjectsGroup;
+import com.echospiral.projectshed.object.item.Item;
 
 public class World {
 
     private Array<WorldObject> objects;
+    private WorldObjectsGroup<Block> blocks;
+    private WorldObjectsGroup<Item> items;
 
     public World() {
         objects = new Array<>();
+        blocks = new WorldObjectsGroup<>();
+        items = new WorldObjectsGroup<>();
     }
 
     public Array<WorldObject> getObjects() {
         return objects;
+    }
+
+    public WorldObjectsGroup<Block> getBlocks() {
+        return blocks;
+    }
+
+    public WorldObjectsGroup<Item> getItems() {
+        return items;
+    }
+
+    public void addObject(WorldObject object) {
+        getObjects().add(object);
+        if (object instanceof Block) blocks.add((Block) object);
+        if (object instanceof Item) items.add((Item) object);
     }
 
     public void tick(float delta) {
