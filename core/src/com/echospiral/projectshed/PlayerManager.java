db996753f5f009c86ab83d1caadcbe268ccccc86
Jeffrey Kyllo <jkyllo@echospiral.com>
2015-01-24 17:40:01 +0000

    Controller swap.

--- core/src/com/echospiral/projectshed/PlayerManager.java
+++ core/src/com/echospiral/projectshed/PlayerManager.java
@@ -10,19 +10,27 @@ import static com.echospiral.projectshed.Role.*;
 public class PlayerManager {
 
     private Array<Player> players;
+    private Array<MappedController> participatingControllers;
 
     public PlayerManager() {
         players = new Array<>();
+        participatingControllers = new Array<>();
     }
 
     public Array<Player> getPlayers() {
         return players;
     }
-    
+
+    public MappedController getControllerForRole(Role role) {
+        int i = role.ordinal() % participatingControllers.size;
+        return participatingControllers.get(i);
+    }
+
     public void assignPlayerController(MappedController controller) {
         for(Player player : players) {
             if(player.getController() == null) {
                 player.setController(controller);
+                participatingControllers.add(controller);
                 return;
             }
         }
@@ -59,15 +67,17 @@ public class PlayerManager {
             switch (player.getRole()) {
                 case PLAYER:
                     player.setRole(BUILDER);
+                    player.setController(getControllerForRole(BUILDER));
                     break;
                 case BUILDER:
                     player.setRole(DESTROYER);
+                    player.setController(getControllerForRole(DESTROYER));
                     break;
                 case DESTROYER:
                     player.setRole(PLAYER);
+                    player.setController(getControllerForRole(PLAYER));
                     break;
             }
         }
     }
-
 }
